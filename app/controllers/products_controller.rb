a43a226dd881c7e6f933be50baf554f75dbce0c4
Daniel Mack <daniel.mack@powerhrg.com>
2018-09-03 08:04:52 -0400

    finished lab

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+
+
   def index
     @products = Product.all
   end
@@ -22,9 +25,22 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def show
+  end
+
+  def data
+    product = Product.find(params[:id])
+    render json: ProductSerializer.serialize(product)
+  end
+
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :inventory, :price)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
